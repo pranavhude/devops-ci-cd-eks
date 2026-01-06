32bd2606373dc4c4df88f7775f238f5355317b01
Pranav Hude <pranavhude20@gmail.com>
2026-01-06 23:23:07 +0530

    Update code

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,42 +1,67 @@
+################################
+# VPC
+################################
 module "vpc" {
-    source = "./modules/vpc"
-    vpc_cidr = var.vpc_cidr
-    public_subnets = var.public_subnets
-    private_subnets = var.private_subnets
-    tags = var.tags
+  source = "./modules/vpc"
+
+  vpc_cidr       = var.vpc_cidr
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+  tags           = var.tags
 }
 
+################################
+# SECURITY GROUPS
+################################
 module "security_groups" {
-    source = "./modules/security-groups"
-    vpc_id = modules.vpc.vpc_id
+  source = "./modules/security-groups"
+
+  vpc_id = module.vpc.vpc_id
 }
 
+################################
+# IAM
+################################
 module "iam" {
-    source = "./modules/eks"
-    cluster_name = var.eks_cluster_name
-    subnet_ids = module.vpc.private_subnet_ids
-    cluster_role_arn = module.iam.eks_cluster_role_arn
+  source = "./modules/iam"
 }
 
+################################
+# EKS CLUSTER
+################################
+module "eks" {
+  source = "./modules/eks"
+
+  cluster_name     = var.eks_cluster_name
+  subnet_ids       = module.vpc.private_subnet_ids
+  cluster_role_arn = module.iam.cluster_role_arn
+}
+
+################################
+# NODE GROUP
+################################
 module "nodegroup" {
   source = "./modules/nodegroup"
 
-  cluster_name    = module.eks.cluster_name
-  node_role_arn  = module.iam.node_role_arn
-
-  subnet_ids = module.vpc.private_subnet_ids
+  cluster_name   = module.eks.cluster_name
+  node_role_arn = module.iam.node_role_arn
+  subnet_ids     = module.vpc.private_subnet_ids
 
   desired_size = 2
   min_size     = 1
   max_size     = 3
 }
 
+################################
+# JENKINS
+################################
 module "jenkins" {
-    source        = "./modules/jenkins"
-    subnet_id     = module.vpc.public_subnet_ids[0]
-    instance_type = var.jenkins_instance_type
-    key_name      = var.jenkins_key_name
-    sg_id         = module_security_groups.jenkins_sg
-    iam_profile   = module.iam.jenkins_instance_profile
-    tags          = var.tags
+  source        = "./modules/jenkins"
+
+  subnet_id     = module.vpc.public_subnet_ids[0]
+  instance_type = var.jenkins_instance_type
+  key_name      = var.jenkins_key_name
+  sg_id         = module.security_groups.jenkins_sg
+  iam_profile   = module.iam.jenkins_instance_profile
+  tags          = var.tags
 }
