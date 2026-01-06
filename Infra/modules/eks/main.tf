3ae6bb977f9b253856def1af84abf43d2f9bcbb9
Pranav Hude <pranavhude20@gmail.com>
2026-01-06 22:47:13 +0530

    updatecode

--- Infra/modules/eks/main.tf
+++ Infra/modules/eks/main.tf
@@ -1,12 +1,23 @@
-variable "cluster_name" {}
-variable "subnet_ids" {}
-variable "cluster_role arn" {}
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs for EKS cluster"
+  type        = list(string)
+}
+
+variable "cluster_role_arn" {
+  description = "IAM role ARN for EKS cluster"
+  type        = string
+}
 
 resource "aws_eks_cluster" "this" {
-    name  = var.cluster_name
-    role_arn  = var.cluster_role_arn
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
 
-    vpc_config {
-        subnet_ids = var.subntes_ids
-    }
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
 }
--- Infra/modules/eks/output.tf
+++ Infra/modules/eks/output.tf
@@ -1,3 +1,19 @@
-output "cluster_nmae" {
-    value = aws_eks_cluster.this.time
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.this.arn
+}
+
+output "cluster_endpoint" {
+  description = "EKS API server endpoint"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority" {
+  description = "EKS cluster CA certificate"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
 }
