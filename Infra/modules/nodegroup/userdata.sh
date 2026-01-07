4c2c3570112d248f8e9a67686bb26ba8247363fb
Pranav Hude <pranavhude20@gmail.com>
2026-01-07 23:47:40 +0530

    updatecode

--- Infra/modules/nodegroup/userdata.sh
+++ Infra/modules/nodegroup/userdata.sh
@@ -1,15 +1,22 @@
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+
+--==MYBOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
+
 #!/bin/bash
-set -ex
+set -euxo pipefail
 
-# EKS Bootstrap (Mandatory)
-/etc/eks/bootstrap.sh $(CLUSTER_NAME)
+####################################
+# EKS Bootstrap (MANDATORY)
+####################################
+/etc/eks/bootstrap.sh ${CLUSTER_NAME}
 
-#Custom Node Setup (Optional)
-
-#Install common tools
+####################################
+# Custom Node Setup
+####################################
 yum install -y git curl jq awscli
-
-# Optional : enable IP forwarding (safe default)
-
 sysctl -w net.ipv4.ip_forward=1
+
+--==MYBOUNDARY==--
 
